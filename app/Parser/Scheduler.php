2cdad748264ab0d1911b387611091baed5cfbbb5
Dmitriy Pereverza <dmitriy.p.ws@gmail.com>
2017-08-29 13:06:03 +0300

    AUT-48.Парсинг данных с сайтов.Рефакторинг

--- app/Parser/Scheduler.php
+++ app/Parser/Scheduler.php
@@ -17,32 +17,45 @@ class Scheduler {
      * @param Parser $parser
      */
     public function run($parser) {
+        if ($this->parserWorks) {
+            return;
+        }
+
         foreach ($parser->getArrayConfig() as $site) {
             if (!$site['active']) {
                 continue;
             }
-            $time = Carbon::now('Europe/Moscow');
-            $dayOfWeek = $time->dayOfWeek;
-            $workTime = $site['work_time'][self::$dayOfWeek[$dayOfWeek - 1]];
-
-            foreach ($workTime as $timeDiff) {
-                /** @var string $time_from */
-                /** @var string $time_to */
-                extract($timeDiff);
-                $timeFromParts = explode(':', $time_from);
-                $timeToParts = explode(':', $time_to);
-
-                $isMatch = $time->between(
-                    Carbon::today('Europe/Moscow')->addHours($timeFromParts[0])->addMinutes($timeFromParts[1]),
-                    Carbon::today('Europe/Moscow')->addHours($timeToParts[0])->addMinutes($timeToParts[1])
-                );
-
-                if ($isMatch) {
+            $nowTime = Carbon::now('Europe/Moscow');
+            foreach ($this->getWorkTimeForCurrentDay($site, $nowTime->dayOfWeek) as $timeDiff) {
+                if ($this->isMatch($nowTime, $timeDiff)) {
                     $this->parserWorks = true;
                     SpiderManager::getSpiderFromConfig($site)->crawl();
                     break;
                 }
             }
         }
+    }
+
+    /**
+     * @param Carbon $time
+     * @param $timeDiff
+     * @return bool
+     */
+    private function isMatch($time, $timeDiff) {
+        extract($timeDiff);
+        /** @var string $time_from */
+        $timeFromParts = explode(':', $time_from);
+        /** @var string $time_to */
+        $timeToParts = explode(':', $time_to);
+        return $time->between(Carbon::today('Europe/Moscow')->addHours($timeFromParts[0])->addMinutes($timeFromParts[1]), Carbon::today('Europe/Moscow')->addHours($timeToParts[0])->addMinutes($timeToParts[1]));
+    }
+
+    /**
+     * @param $siteConfig
+     * @param $currentDayOfWeek
+     * @return array
+     */
+    private function getWorkTimeForCurrentDay($siteConfig, $currentDayOfWeek) {
+        return $siteConfig['work_time'][self::$dayOfWeek[$currentDayOfWeek - 1]];
     }
 }
--- app/Parser/Spider/DefaultSpider.php
+++ app/Parser/Spider/DefaultSpider.php
@@ -6,11 +6,12 @@ use Symfony\Component\EventDispatcher\Event;
 use VDB\Spider\Discoverer\XPathExpressionDiscoverer;
 use VDB\Spider\Event\SpiderEvents;
 use VDB\Spider\EventListener\PolitenessPolicyListener;
-use VDB\Spider\PersistenceHandler\PersistenceHandlerInterface;
 use VDB\Spider\Spider;
 use VDB\Spider\StatsHandler;
 
 class DefaultSpider implements SpiderInterface {
+    /** @var Spider $spider  */
+    private $spider;
     private $itemsListSelector;
     private $itemsListUrl;
     private $isActive;
@@ -21,50 +22,42 @@ class DefaultSpider implements SpiderInterface {
         $this->isActive = $config['active'];
         $this->itemsListSelector = $config['items_list_selector'];
         $this->selectors = $config['selectors'];
+        $this->spider = $this->getSpider();
+        $this->setHandlers();
     }
 
     public function crawl() {
+        $this->spider->crawl();
+
+        $this->saveInTemporaryTable();
+    }
+
+    public function stop() {
+        // TODO: Implement stop() method.
+    }
+
+    private function getSpider() {
         $spider = new Spider($this->itemsListUrl);
         $spider->getDiscovererSet()->set(new XPathExpressionDiscoverer($this->itemsListSelector));
         $spider->getDiscovererSet()->maxDepth = 1;
         $spider->getQueueManager()->maxQueueSize = 10;
+    }
 
-        $spider->getDispatcher()->addListener(
+    private function setHandlers() {
+        $this->spider->getDispatcher()->addListener(
             SpiderEvents::SPIDER_CRAWL_USER_STOPPED,
             function (Event $event) {
                 echo "\nCrawl aborted by user.\n";
                 exit();
             }
         );
-        $politenessPolicyEventListener = new PolitenessPolicyListener(100);
-        $spider->getDownloader()->getDispatcher()->addListener(
+        $this->spider->getDownloader()->getDispatcher()->addListener(
             SpiderEvents::SPIDER_CRAWL_PRE_REQUEST,
-            [$politenessPolicyEventListener, 'onCrawlPreRequest']
+            [new PolitenessPolicyListener(100), 'onCrawlPreRequest']
         );
-
         $statsHandler = new StatsHandler();
-        $spider->getQueueManager()->getDispatcher()->addSubscriber($statsHandler);
-        $spider->getDispatcher()->addSubscriber($statsHandler);
-
-        $spider->crawl();
-
-        echo "\n  ENQUEUED:  " . count($statsHandler->getQueued());
-        echo "\n  SKIPPED:   " . count($statsHandler->getFiltered());
-        echo "\n  FAILED:    " . count($statsHandler->getFailed());
-        echo "\n  PERSISTED:    " . count($statsHandler->getPersisted());
-        echo "\n\nDOWNLOADED RESOURCES: ";
-
-        foreach ($spider->getDownloader()->getPersistenceHandler() as $resource) {
-            echo 'Страница: ' . $this->getPageUrl($resource) . "\n";
-            echo 'Бренд: ' . $this->getSelectorContent($resource, $this->selectors['brand']) . "\n";
-            echo 'Модель: ' . $this->getSelectorContent($resource, $this->selectors['model']) . "\n";
-            echo 'Деталь: ' . $this->getSelectorContent($resource, $this->selectors['detail']) . "\n";
-            echo "\n";
-        }
-    }
-
-    public function stop() {
-        // TODO: Implement stop() method.
+        $this->spider->getQueueManager()->getDispatcher()->addSubscriber($statsHandler);
+        $this->spider->getDispatcher()->addSubscriber($statsHandler);
     }
 
     private function getSelectorContent($resource, $selector) {
@@ -76,5 +69,16 @@ class DefaultSpider implements SpiderInterface {
 
     private function getPageUrl($resource) {
         return $resource->getCrawler()->getUri();
+    }
+
+    private function saveInTemporaryTable() {
+        // TODO remove console output
+        foreach ($this->spider->getDownloader()->getPersistenceHandler() as $resource) {
+            echo 'Url: ' . $this->getPageUrl($resource) . "\n";
+            foreach ($this->selectors as $key => $selector) {
+                echo $key . ': ' . $this->getSelectorContent($resource, $selector) . "\n";
+            }
+            echo "\n";
+        }
     }
 }
