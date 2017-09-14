489822fbeeea1838349ffa03946737fcd0372269
Dmitriy Pereverza <dmitriy.p.ws@gmail.com>
2017-09-14 14:18:59 +0300

    AUT-48 Парсинг данных с сайтов.Удаление стандартных контроллеров, добавление валидации для апи

--- app/Http/Controllers/Parser/ParserController.php
+++ app/Http/Controllers/Parser/ParserController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers\Parser;
 use App\Http\Controllers\Controller;
 use App\Models\TemporarySearchResults;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ParserController extends Controller {
     public function getVersion() {
         return json_encode([
-            'currentVersion' => TemporarySearchResults::getCurrentVersion()
+            "currentVersion" => TemporarySearchResults::getCurrentVersion()
         ]);
     }
 
     public function getResource(Request $request) {
+        $validation = Validator::make($request->all(),[
+            'self_version' => 'required|numeric',
+        ]);
+        if($validation->fails()){
+            return $validation->errors()->toArray();
+        }
         try {
             $arRequest = json_decode($request->getContent(), true);
             $version = $arRequest['self_version'];
