3bcee6c307d19f15c89119588c6e707aeb72dd54
Dmitriy Pereverza <dmitriy.p.ws@gmail.com>
2017-09-14 13:13:28 +0300

    AUT-48 Парсинг данных с сайтов.IDE helper для корректного отображения фасадов Laravel

--- app/Listeners/ParserInfoListener.php
+++ app/Listeners/ParserInfoListener.php
@@ -3,11 +3,7 @@ namespace App\Listeners;
 
 use App\Events\ParserInfoEvent;
 
-class ParserInfoListener
-{
-    public function __construct() {
-    }
-
+class ParserInfoListener {
     public function handle(ParserInfoEvent $event) {
         app('console.logger')->info($event->getText(), $event->getContext());
         app('file.logger')->info($event->getText(), $event->getContext());
